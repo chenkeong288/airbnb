28d0ad0c26db49bc34d9ba6e9e7f6fdd05d9ef16
chenkeong <chenkeong@chenkeongs-MacBook-Air.local>
2018-02-28 18:59:40 +0800

    fixed migration file

--- db/migrate/20180213125231_add_time_stamps.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeStamps < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :listings, :created_at, :datetime, null: false
-  	add_column :listings, :updated_at, :datetime, null: false
-  end
-end
