445f02cef0e74162cb666c76d1881b08c859ac91
chenkeong288 <chenkeong288@gmail.com>
2018-03-06 17:33:44 +0800

    made payment based on total price

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,26 +4,32 @@ class PaymentsController < ApplicationController
     @client_token = Braintree::ClientToken.generate
   end
 
+
   def checkout
+
+    #Total price
+    @reservation = Reservation.find_by(id: params[:id])
+
     nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
 
     result = Braintree::Transaction.sale(
-     :amount => "100.00", #this is currently hardcoded
+     :amount => @reservation.total_price, 
      :payment_method_nonce => nonce_from_the_client,
      :options => {
         :submit_for_settlement => true
       }
      )
 
+
     if result.success?
+
+      @reservation.payment_status = "Yes"                     # Make payment_status of the reservation in the Reservation listing "yes"
+      @reservation.save                                       # Save it
+
       redirect_to listings_path, :flash => { :success => "Transaction successful!" }
     else
       redirect_to listings_path, :flash => { :error => "Transaction failed. Please try again." }
     end
-  end
-
-  def show
-
   end
 
 end
